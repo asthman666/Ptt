cb775e9716681fbbca3b4669f3e2aed65a75f90e
xiaonan song <4king-gruuby@163.com>
2013-01-20 08:58:33 +0800

    default q.op is 'AND'

--- lib/Ptt/Model/API/Search.pm
+++ lib/Ptt/Model/API/Search.pm
@@ -21,7 +21,7 @@ sub search {
     if ( $qh->{ean} ) {
 	$path = "/solr/collection1/select?q=ean:$qh->{ean}&wt=json";
     } elsif ( $qh->{k} ) {
-	$path = "/solr/collection1/select?q=title:" . uri_escape_utf8($qh->{k}) . "&wt=json";
+	$path = "/solr/collection1/select?q=title:" . uri_escape_utf8($qh->{k}) . "&wt=json&q.op=AND";
     }
 
     if ( $qh->{p} ) {
